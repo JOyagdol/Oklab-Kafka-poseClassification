3cca1dac2992a1b6c544009d64b4fc1a6202c272
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2025-05-20 13:06:20 +0900

    first commit

---
+++ app/src/main/java/pose/classification/Info.java
@@ -0,0 +1,12 @@
+package pose.classification;
+
+public class Info {
+    // String kafka5 = "10.3.128.33:60011"; //9092
+    String kafka1 = "192.168.99.47:9092";
+    String kafka2 = "192.168.99.48:9092";
+    String kafka3 = "192.168.99.49:9092";
+    String topicname = "pose-classification-event";
+    String url = "mongodb://root:gamelab2)5@10.3.128.35:14407"; //"10.3.129.33:27017" 
+    String database = "pose-classification-event";
+    String collection = "pose-classification-event";
+}
